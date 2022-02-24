dad489fb28ba82225b40659b512a111a1ce41f09
snrahul11 <snrahul11@gmail.com>
2022-02-24 15:10:03 +0530

    Worked on program for creating ppt with images

--- src/main/java/com/codingjump/App.java
+++ src/main/java/com/codingjump/App.java
@@ -1,6 +1,10 @@
 package com.codingjump;
 
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Collection;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -8,16 +12,24 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.poi.sl.usermodel.PictureData;
+import org.apache.poi.util.IOUtils;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
 
 public class App {
+    static Options options;
+
+    static String exportOption = "export";
+
     public static CommandLine parseArguments(String[] args) throws ParseException {
-        HelpFormatter formatter = new HelpFormatter();
-
-        Options options = new Options();
-        options.addOption("e", "export", true, "Exports powerpoint file with given name");
-
-        formatter.printHelp("pp-helper", options);
+        options = new Options();
+        options.addOption("e", exportOption, true, "Exports powerpoint file with given name");
+        options.addOption("d", "dir", true, "Location of directory where images are stored");
+        options.addOption("r", "run", false, "If argment present then it will execute otherwise not");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd;
@@ -29,16 +41,56 @@ public class App {
     public static void main(String[] args) throws Exception {
         var cmd = parseArguments(args);
 
-        try (var ppt = new XMLSlideShow()) {
-            ppt.createSlide();
+        String outFileLocation;
+        String dirLocation;
+        boolean run = false;
+        if (cmd.hasOption(exportOption))
+            outFileLocation = cmd.getOptionValue("export") + ".pptx";
+        else
+            outFileLocation = "output.pptx";
 
-            FileOutputStream out;
-            if (cmd.hasOption("export"))
-                out = new FileOutputStream(cmd.getOptionValue("export") + ".pptx");
-            else
-                out = new FileOutputStream("output.pptx");
-            ppt.write(out);
-            out.close();
+        if (cmd.hasOption("dir"))
+            dirLocation = cmd.getOptionValue("dir");
+        else
+            dirLocation = "./";
+
+        if (cmd.hasOption("run"))
+            run = true;
+
+        if (!run) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("pp-helper", options);
+            return;
+        }
+
+        try (var ppt = new XMLSlideShow()) {
+
+            File dir;
+            try (FileOutputStream outFile = new FileOutputStream(outFileLocation)) {
+                dir = new File(dirLocation);
+
+                Collection<File> files = FileUtils.listFiles(
+                        dir,
+                        new RegexFileFilter("^(.*?)(.jpg|.png|.svg|.jpeg|.gif)"),
+                        DirectoryFileFilter.DIRECTORY);
+
+                // Files imported
+                for (var file : files) {
+                    var slide = ppt.createSlide();
+                    byte[] pictureData = IOUtils.toByteArray(new FileInputStream(file));
+                    XSLFPictureData pd = ppt.addPicture(pictureData, PictureData.PictureType.PNG);
+                    var picture = slide.createPicture(pd);
+                    var dimensions = slide.getSlideShow().getPageSize();
+                    var anchor = new Rectangle2D.Float(0f, 0f, (float) dimensions.getWidth(),
+                            (float) dimensions.getHeight());
+                    picture.setAnchor(anchor);
+                    System.out.println("Image imported: " + file);
+                }
+
+                ppt.write(outFile);
+                // Exported file
+                System.out.println("Exported file: " + outFileLocation);
+            }
         }
     }
 }
